a9080383982307c2316696f978606543d659fb07
MelonMania <dnwlsskfn@gmail.com>
2021-03-07 05:06:56 +0900

    complete delegate Setting

---
+++ Clima-iOS13/Clima/Models/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by RooZin on 2021/03/07.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima-iOS13/Clima/Models/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by RooZin on 2021/03/06.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
