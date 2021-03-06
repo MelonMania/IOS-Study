ed3837f03b6974ebe470c9e023c455a89000a210
MelonMania <dnwlsskfn@gmail.com>
2021-03-07 05:08:13 +0900

    JSON decoding function complete

--- Clima-iOS13/Clima/Models/WeatherManager.swift
+++ Clima-iOS13/Clima/Models/WeatherManager.swift
@@ -1,9 +1,40 @@
-//
-//  WeatherManager.swift
-//  Clima
-//
-//  Created by RooZin on 2021/03/06.
-//  Copyright © 2021 App Brewery. All rights reserved.
-//
+import UIKit
 
-import Foundation
+struct WeatherManager {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=664f067cac70f1ddb9a88dd5fb8b6d3e&units=metric"
+    
+    mutating func fetchURL(cityName : String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString : String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData : Data)  {
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        }
+        catch{
+            print(error)
+        }
+        
+    }
+    
+}
